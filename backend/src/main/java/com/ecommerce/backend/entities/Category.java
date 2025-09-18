11bed09425e1669bae1aa7dcf4aaf6f081eb653e
printf-Shivam <shivamkrsingh264@gmail.com>
2025-09-18 22:22:25 +0530

    Create Category.java

---
+++ backend/src/main/java/com/ecommerce/backend/entities/Category.java
@@ -0,0 +1,37 @@
+package com.ecommerce.backend.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "categories")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Category {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String code;
+
+    @Column(nullable = false)
+    private String description;
+
+    @OneToMany(mappedBy = "category",cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonManagedReference
+    private List<CategoryType> categoryType;
+}
