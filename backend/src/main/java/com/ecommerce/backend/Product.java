70d38dce6cd5a17c183ae346a20e1591a48333b7
printf-Shivam <shivamkrsingh264@gmail.com>
2025-09-18 22:18:32 +0530

    Create Product.java

---
+++ backend/src/main/java/com/ecommerce/backend/Product.java
@@ -0,0 +1,77 @@
+package com.ecommerce.backend.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "products")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Product {
+    @Column
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column
+    private String description;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    @Column(nullable = false)
+    private String brand;
+
+    @Column
+    private Float rating;
+
+    @Column(nullable = false)
+    private boolean isNewArrival;
+
+    @Column(nullable = false, updatable = true)
+    @Temporal(TemporalType.TIMESTAMP)
+    private java.util.Date createdAt;
+
+    @Column(nullable = false, updatable = true)
+    @Temporal(TemporalType.TIMESTAMP)
+    private java.util.Date updatedAt;
+
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL)
+    private List<ProductVariant> productVariants;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id",nullable = false)
+
+    private Category category;
+
+    @ManyToOne
+    @JoinColumn(name = "categoryType_id",nullable = false)
+    private CategoryType categoryType;
+
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL)
+    private List<Resources> resources;
+    @PrePersist()
+    protected void onCreate(){
+        createdAt= new java.util.Date();
+        updatedAt=createdAt;
+    }
+
+    @PreUpdate()
+    protected void onUpdate(){
+        updatedAt = new java.util.Date();
+    }
+
+}
